c91ae055ff3a1b9531e6dc8101a56ebf7bfd764e
Leonardo Vasconcelos <leobav1988@gmail.com>
2025-10-15 04:01:37 -0300

    .

--- app/Filament/StageControl/Resources/ProductionResource.php
+++ app/Filament/StageControl/Resources/ProductionResource.php
@@ -9,6 +9,7 @@ use App\Models\Production;
 use App\Models\ProductionGrid;
 use App\Models\Size;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Support\Enums\FontWeight;
@@ -16,6 +17,7 @@ use Filament\Tables;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -49,23 +51,84 @@ class ProductionResource extends Resource
             ];
         });
 
+        $statuses = $statuses->except([ProductionStatusEnum::Pending->value, ProductionStatusEnum::Completed->value]);
+
         return $table
             ->columns([
-                TextColumn::make('product.name')->label(__('resources.productions.table.product'))->description(fn (Production $record) => $record->color->title)->weight(FontWeight::Bold)->sortable(),
+                TextColumn::make('product.name')->label(__('resources.productions.table.product'))->description(fn (Production $record) => $record->color->title)->weight(FontWeight::Bold)->searchable(),
                 ...$sizes->map(function (Size $size) {
                     return TextColumn::make('size_'.$size->alias)->label($size->alias)->default(0);
                 }),
                 //TextColumn::make('total_qty')->label(__('resources.productions.table.total_qty'))->default(0),
                 TextColumn::make('status')->label(__('resources.productions.table.status'))->badge()
                     ->getStateUsing(fn (Production $record) => __('enums.production_status.'.$record->status->value))
-                    ->color(fn (Production $record) => $record->status->color()),
+                    ->color(fn (Production $record) => $record->status->color())
+                    /*->sortable(query: function (Builder $query, string $direction): Builder {
+                        return $query->orderByRaw(
+                            "CASE 
+                                WHEN date_completed IS NOT NULL THEN 5
+                                WHEN date_finishing IS NOT NULL THEN 4
+                                WHEN date_sewing IS NOT NULL THEN 3
+                                WHEN date_cutting IS NOT NULL THEN 2
+                                ELSE 1
+                             END {$direction}"
+                        );
+                    }),*/
                 //TextColumn::make('client.name')->label(__('resources.productions.table.client'))->sortable(),
                 //TextColumn::make('cutter.name')->label(__('resources.productions.table.cutter'))->sortable(),
                 //TextColumn::make('date_started')->label(__('resources.productions.table.date_started'))->date('d/m/Y')->sortable(),
 
             ])
             ->filters([
+                Filter::make('status')
+                    ->form([
+                        Select::make('status')->options($statuses)->label(__('resources.productions.table.filter.status'))->multiple(),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        $query->where(function (Builder $query) use ($data) {                           
+
+                            if(in_array(ProductionStatusEnum::Finishing->value, $data['status'])) {
+                                $query->orWhere(function (Builder $query) {
+                                    $query->whereNotNull('date_finishing');
+                                    $query->whereNull('date_completed');
+                                });
+                            }
+
+                            if(in_array(ProductionStatusEnum::Sewing->value, $data['status'])) {
+                                $query->orWhere(function (Builder $query) {
+                                    $query->whereNotNull('date_sewing');
+                                    $query->whereNull('date_finishing');
+                                });
+                            }
+
+                            if(in_array(ProductionStatusEnum::Cutting->value, $data['status'])) {
+                                $query->orWhere(function (Builder $query) {
+                                    $query->whereNotNull('date_cutting');
+                                    $query->whereNull('date_sewing');
+                                });
+                            }
+                            
+                        });
+
+                        return $query;
+                            
+                    })->indicateUsing(function (array $data) use ($statuses): string {
+                        if (! $data['status']) {
+                            return '';
+                        }
+
+                        $values = collect($data['status'])->map(function ($status) use ($statuses) {
+                            return $statuses[$status];
+                        });
+
+                        return __('resources.productions.table.filter.status') . ': ' . implode(', ',  $values->toArray());
+                    }),
             ])
+            ->filtersTriggerAction(
+                fn (Action $action) => $action
+                    ->button()
+                    ->label(__('resources.productions.table.filter.button')),
+            )
             ->actions([
                 Action::make('previus')->label('')->icon('fas-arrow-left')->color('primary')->button()
                 ->action(fn (Production $record) => $record->previusStep())
@@ -94,7 +157,15 @@ class ProductionResource extends Resource
                 $query->whereNotNull('date_cutting');
                 $query->whereNull('date_completed');
                                       
-                return $query->orderBy('created_at', 'desc');
+                return $query->orderByRaw(
+                    "CASE 
+                        WHEN date_completed IS NOT NULL THEN 5
+                        WHEN date_finishing IS NOT NULL THEN 4
+                        WHEN date_sewing IS NOT NULL THEN 3
+                        WHEN date_cutting IS NOT NULL THEN 2
+                        ELSE 1
+                     END asc"
+                );
             })->paginated(false);
     }
 
