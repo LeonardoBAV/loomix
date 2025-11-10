45b89cbd18f7568370a3198189f139c9617c1482
LeonardoBAV <leobav1988@gmail.com>
2025-11-10 12:10:29 -0300

    Refactor getNavigationGroup method formatting

--- app/Filament/Resources/TrimResource.php
+++ app/Filament/Resources/TrimResource.php
@@ -28,8 +28,7 @@ class TrimResource extends Resource
 
     protected static ?int $navigationSort = 3;
 
-    public static function getNavigationGroup(): string
-    {
+    public static function getNavigationGroup(): string{
         return __('Supplies');
     }
     
