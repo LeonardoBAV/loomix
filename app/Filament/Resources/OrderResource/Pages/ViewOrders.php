b4fe496498e004e9664f920fce04a34ba8a9dc7c
Leonardo Vasconcelos <leobav1988@gmail.com>
2025-11-11 01:04:10 -0300

    Redeclare Tal

--- app/Filament/Resources/OrderResource/Pages/ViewOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrders.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\Resources\ProductResource\Pages;
+namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
 use Filament\Actions\EditAction;
